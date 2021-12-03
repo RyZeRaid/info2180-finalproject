da59aa7ea7c8816af893256563bc195f2d66eee7
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2021-12-02 20:02:58 -0500

    Edited dashboard.php

--- scripts/dashboard.php
+++ scripts/dashboard.php
@@ -47,7 +47,6 @@ if($context === "all"){
 
 if($context === "open"){
     $stmt = $conn->query("SELECT * FROM issues WHERE status = '%$status%'");
-
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo "<table id = 'info' border =\"1\" style='border-collapse: collapse'>";
@@ -77,31 +76,48 @@ if($context === "open"){
 if($context === "myticket"){
     $stnt = $conn->query("SELECT * FROM user WHERE  eamil = '$_SESION['email']'"); 
     $name = $stnt->fetchAll(PDO::FETCH_ASSOC);
+    
 
     $stmt = $conn->query("SELECT * FROM issue WHERE  assigned_to = '$name['id']'"); 
-    
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $resulted = mysqli_fetch_assoc($stmt);
+    $cnt=$stmt->num_rows;
 
-    echo "<table id = 'info' border =\"1\" style='border-collapse: collapse'>";
-    echo "<tr>";
-    echo "<th>Title</th>";
-    echo "<th>Type</th>";
-    echo "<th>Status</th>";
-    echo "<th>Assigned To</th>";
-    echo "<th>Created</th>";
-    echo "</tr>";
-
-    foreach ($results as $row){
-        $stst = $conn->query("SELECT firstname,lastname FROM user WHERE id = '$row['assigned_to']'");
-        $return_name = $stst->fetchAll(PDO::FETCH_ASSOC);
-        
-        echo "<tr> \n";
-        echo "<td>#" .$row['id']..$row['title']. "</td> \n";
-        echo "<td>" .$row['type']. "</td> \n";
-        echo "<td>" .$row['status']. "</td> \n";
-        echo "<td>" $return_name "</td> \n";
-        echo "<td>" .$row['created']. "</td> \n";
+    if($cnt!=0){
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo "<table id = 'info' border =\"1\" style='border-collapse: collapse'>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Type</th>";
+        echo "<th>Status</th>";
+        echo "<th>Assigned To</th>";
+        echo "<th>Created</th>";
         echo "</tr>";
+
+        foreach ($results as $row){
+            $stst = $conn->query("SELECT firstname,lastname FROM user WHERE id = '$row['assigned_to']'");
+            $return_name = $stst->fetchAll(PDO::FETCH_ASSOC);
+            
+            echo "<tr> \n";
+            echo "<td>#" .$row['id']..$row['title']. "</td> \n";
+            echo "<td>" .$row['type']. "</td> \n";
+            echo "<td>" .$row['status']. "</td> \n";
+            echo "<td>" $return_name "</td> \n";
+            echo "<td>" .$row['created']. "</td> \n";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }else{
+        echo "<table id = 'info' border =\"1\" style='border-collapse: collapse'>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Type</th>";
+        echo "<th>Status</th>";
+        echo "<th>Assigned To</th>";
+        echo "<th>Created</th>";
+        echo "</tr>";
+        echo "</table>";
+
     }
-    echo "</table>";
+
+    
 }
